bc644458b897a18a4286d8c6f46440924ae4b7ee
rachitraam <rachit.guruprasad@gmail.com>
2025-03-14 12:06:05 +0530

    Webhook task4

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this again(webhook stuff attempt "
-          "4.0)\n";
+  cout << "I have successfully built and run this again(Task 4) \n";
   return 0;
 }
